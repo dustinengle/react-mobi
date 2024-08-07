0457d4a7b9b790916a22e91f776ae053b456a4ad
dustinengle <dustin.engle@gmail.com>
2024-08-07 16:13:29 -0700

    refactor formatter

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,42 +1,25 @@
 
 export class Formatter {
-  static _currency: Intl.NumberFormat = new Intl.NumberFormat('en-US', {
-    currency: 'USD',
-    style: 'currency',
-  })
-  static _percentage: Intl.NumberFormat = new Intl.NumberFormat('en', {
-    maximumFractionDigits: 4,
-    minimumFractionDigits: 4,
-    style: 'percent',
-  })
-
   static currency(n: number): string {
-    return this._currency.format(n)
+    return new Intl.NumberFormat('en-US', {
+      currency: 'USD',
+      style: 'currency',
+    }).format(n)
   }
 
-  static percentage(n: number): string {
-    return this._percentage.format(n)
-  }
-}
-
-export class ShortFormatter {
-  static _currency: Intl.NumberFormat = new Intl.NumberFormat('en-US', {
-    currency: 'USD',
-    maximumFractionDigits: 0,
-    minimumFractionDigits: 0,
-    style: 'currency',
-  })
-  static _percentage: Intl.NumberFormat = new Intl.NumberFormat('en', {
-    maximumFractionDigits: 0,
-    minimumFractionDigits: 0,
-    style: 'percent',
-  })
-
-  static currency(n: number): string {
-    return this._currency.format(n)
+  static number(n: number): string {
+    return new Intl.NumberFormat('en-US', {
+      currency: 'USD',
+      maximumFractionDigits: 0,
+      minimumFractionDigits: 0,
+    }).format(n)
   }
 
-  static percentage(n: number): string {
-    return this._percentage.format(n)
+  static percentage(n: number, digits: number = 4): string {
+    return new Intl.NumberFormat('en', {
+      maximumFractionDigits: digits,
+      minimumFractionDigits: digits,
+      style: 'percent',
+    }).format(n)
   }
 }
