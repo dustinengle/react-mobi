1005135dcc4ac891a76732cd67f1b00fcab9fdc5
dustinengle <dustin.engle@gmail.com>
2024-08-07 16:08:17 -0700

    add short formatter

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -18,3 +18,25 @@ export class Formatter {
     return this._percentage.format(n)
   }
 }
+
+export class ShortFormatter {
+  static _currency: Intl.NumberFormat = new Intl.NumberFormat('en-US', {
+    currency: 'USD',
+    maximumFractionDigits: 0,
+    minimumFractionDigits: 0,
+    style: 'currency',
+  })
+  static _percentage: Intl.NumberFormat = new Intl.NumberFormat('en', {
+    maximumFractionDigits: 0,
+    minimumFractionDigits: 0,
+    style: 'percent',
+  })
+
+  static currency(n: number): string {
+    return this._currency.format(n)
+  }
+
+  static percentage(n: number): string {
+    return this._percentage.format(n)
+  }
+}
